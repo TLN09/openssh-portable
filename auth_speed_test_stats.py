03a407600355a42c776e4d02bf68d57fbd4c6ed0
TLN09 <59337970+TLN09@users.noreply.github.com>
2026-01-07 14:02:33 +0100

    E2E authentication speed test

--- auth_speed_test_stats.py
+++ auth_speed_test_stats.py
@@ -16,29 +16,33 @@ def parse_results(data: str) -> list[float]:
     
     return result
 
-def calculate_stats(data: list[float]) -> None:
+def calculate_stats(data: list[float], type: str, include_header: bool) -> None:
     data.sort()
     minimum = data[0]
-    maximum = data[1]
+    maximum = data[-1]
     median = data[len(data) // 2]
     mean = sum(data) / len(data)
     variance = sum(pow(d - mean, 2) for d in data) / len(data)
     std_deviation = sqrt(variance)
-    print('min,max,median,mean,variance,std. dev.')
-    print(f'{minimum},{maximum},{median},{mean},{variance},{std_deviation}')
+    if include_header:
+        print('type,min,max,median,mean,variance,std. dev.')
+    
+    print(f'{type},{minimum},{maximum},{median},{mean},{variance},{std_deviation}')
 
 
-def main(results: str):
+def main(results: str, type: str, include_header: bool):
     with open(results, 'r', encoding='utf-8') as f:
         data = f.read()
 
     data = parse_results(data)
-    calculate_stats(data)
+    calculate_stats(data, type, include_header)
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
     arg_parser = ArgumentParser()
-    arg_parser.add_argument('--results', help='File containing results from test', type=str)
+    arg_parser.add_argument('--results', help='File containing results from test', type=str, required=True)
+    arg_parser.add_argument('--type', help='The type of test conducted', type=str, required=True)
+    arg_parser.add_argument('-i', '--include-header', help='Whether or not to include the initial CSV header', action='store_true', required=False)
     args = arg_parser.parse_args()
     
-    main(args.results)
+    main(args.results, args.type, args.include_header)
--- test_connection_speed.sh
+++ test_connection_speed.sh
@@ -1,11 +1,11 @@
 make -j 4
 clear
-printf "" > $1
-for i in $(seq 1 $(($2 - 1)));
+printf "" > $2
+for i in $(seq 1 $(($3 - 1)));
 do
-    printf "%d\n" $i >> $1
-    ./ssh dhbkmanager exit 2>> $1
+    printf "%d\n" $i >> $2
+    ./ssh $1 exit 2>> $2
     sleep 1
 done
-printf "%d\n" $2 >> $1
-./ssh dhbkmanager exit 2>> $1
+printf "%d\n" $3 >> $2
+./ssh $1 exit 2>> $2
